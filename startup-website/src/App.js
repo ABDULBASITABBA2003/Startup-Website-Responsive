e70bcc4ed6010c3723cbd38b03d5ee5a77f3696f
ABDULBASITABBA2003 <abdulbasitabba16@gmail.com>
2025-04-14 07:27:45 -0700

    Home oa added

--- startup-website/src/App.js
+++ startup-website/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";  // Import the Navbar
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    // components to render to the browser
+    // pages to render to the browser
+    <Router>
+      <Navbar />  {/* Navbar appears on all pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
